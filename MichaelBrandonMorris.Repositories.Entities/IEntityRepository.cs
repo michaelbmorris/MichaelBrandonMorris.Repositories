f5ee1c19cf4857c6639d3e5a37419b9fba2bcc32
Michael Brandon Morris <mmorris@courtcall.com>
2019-03-15 17:10:39 -0400

    Add entities project with interfaces

---
+++ MichaelBrandonMorris.Repositories.Entities/IEntityRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MichaelBrandonMorris.Entities;
+
+namespace MichaelBrandonMorris.Repositories.Entities
+{
+    public interface IEntityRepository<TEntity, TId> where TEntity : Entity<TId>
+    {
+        ISet<TEntity> All();
+        TEntity FindById(TId id);
+        int Create(TEntity entity);
+        int Update(TEntity entity);
+        int Delete(TEntity entity);
+    }
+
+    public interface IEntityRepositoryAsync<TEntity, TId> where TEntity : Entity<TId>
+    {
+        Task<TEntity> FindByIdAsync(TId id);
+        Task<ISet<TEntity>> AllAsync();
+        Task<int> CreateAsync(TEntity entity);
+        Task<int> UpdateAsync(TEntity entity);
+        Task<int> DeleteAsync(TEntity entity);
+    }
+}
--- MichaelBrandonMorris.Repositories.Sql/Repository.cs
+++ MichaelBrandonMorris.Repositories.Sql/Repository.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data;
 using System.Data.SqlClient;
+using MichaelBrandonMorris.Repositories.Configuration;
 
 namespace MichaelBrandonMorris.Repositories.Sql
 {
