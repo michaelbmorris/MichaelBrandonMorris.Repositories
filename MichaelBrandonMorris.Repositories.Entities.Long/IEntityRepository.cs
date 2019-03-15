8add366f1c52125ece0d9ada16e1e67f227e603c
Michael Brandon Morris <mmorris@courtcall.com>
2019-03-15 18:17:10 -0400

    Add long entity repositories

---
+++ MichaelBrandonMorris.Repositories.Entities.Long/IEntityRepository.cs
@@ -0,0 +1,12 @@
+﻿using MichaelBrandonMorris.Entities.Long;
+
+namespace MichaelBrandonMorris.Repositories.Entities.Long
+{
+    public interface IEntityRepository<TEntity> : IEntityRepository<TEntity, long> where TEntity : Entity
+    {
+    }
+
+    public interface IEntityRepositoryAsync<TEntity> : IEntityRepositoryAsync<TEntity, long> where TEntity : Entity
+    {
+    }
+}
